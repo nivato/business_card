24ebd29a0c931f12a76715280dec5d3bef6b972a
Nazar Ivato <nazarivato@gmail.com>
2014-12-15 16:43:08 +0200

    Added navigation and some logo

--- js/helio_tema.js
+++ js/helio_tema.js
@@ -5,11 +5,53 @@
         $locationProvider.html5Mode(true);
         $routeProvider
             .when('/', {templateUrl: '/templates/main.html'})
+            .when('/services', {templateUrl: '/templates/services.html'})
+            .when('/aboutus', {templateUrl: '/templates/aboutus.html'})
+            .when('/contacts', {templateUrl: '/templates/contacts.html'})
             .otherwise({templateUrl: '/templates/main.html'});
     }]);
 
     app.controller('ApplicationController', [function(){
-        alert('Yey!');
+        console.log('Yey!');
     }]);
+    
+    app.controller('NavigationController', ['$scope', '$location', function($scope, $location){
+        var navbar = this;
+        this.tab = 'none';
+        this.refresh = function(){
+            switch($location.path()) {
+                case '/':
+                    this.tab = 'none';
+                    break;
+                case '/services':
+                    this.tab = 'services';
+                    break;
+                case '/aboutus':
+                    this.tab = 'aboutus';
+                    break;
+                case '/contacts':
+                    this.tab = 'contacts';
+                    break;
+                default:
+                    this.tab = 'none';
+            }
+        };
+        this.isSelceted = function(checkTab){
+            return this.tab === checkTab;
+        };
+        $scope.$on('$locationChangeSuccess', function(event, data){
+            navbar.refresh();
+        });
+        this.refresh();
+    }]);
+
+    app.directive('navigationBar', function(){
+        return {
+            restrict: 'E',
+            templateUrl: '/templates/navigation-bar.html',
+            controller: 'NavigationController',
+            controllerAs: 'nav'
+        };
+    });
 
 })();
